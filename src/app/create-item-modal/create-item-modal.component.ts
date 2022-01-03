7ebe233ff26e3aaa0438721580fec5fa1d81b9ba
Nika Barsukov <35861417+Nika2212@users.noreply.github.com>
2022-01-03 12:41:59 +0400

    first commit

---
+++ src/app/create-item-modal/create-item-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'syll-create-item-modal',
+  templateUrl: './create-item-modal.component.html',
+  styleUrls: ['./create-item-modal.component.css']
+})
+export class CreateItemModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
